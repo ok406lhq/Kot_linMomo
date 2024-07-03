17b4e96e2ece8fcff430613d30c760bd068a87ee
lhq <ok406lhq@163.com>
2024-07-03 18:11:05 +0800

    KotLin Learning 02

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,9 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-//sampleStart
-val PI = 3.14
-var x = 0
-fun incrementX() {
-    x += 1
-}
-// x = 0; PI = 3.14
-// incrementX()
-// x = 1; PI = 3.14
-//sampleEnd
-
 fun main() {
-    println("x = $x; PI = $PI")
-    incrementX()
-    println("incrementX()")
-    println("x = $x; PI = $PI")
+//sampleStart
+    for (x in 1..5) {
+        print(x)
+    }
+//sampleEnd
 }
